c238b5e0281d827a5edf99fc2c9e0264d438fbc0
Bhavesh Pandey <bhavesh.hpandey@gmail.com>
2018-10-07 13:56:52 +0530

    filtered

--- SortingApp/myflaskapp/app.py
+++ SortingApp/myflaskapp/app.py
@@ -11,6 +11,8 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
+#data for forms
+
 class SortingAction(Form):
     sourcefile = StringField('Source path and filename',[validators.Length(min = 1)])
     sheetname = StringField('Sheet Name',[validators.Length(min = 1)])
@@ -25,6 +27,7 @@ def start():
     form = SortingAction(request.form)
 
     if request.method == 'POST' and form.validate():
+        #fetching the data
         sourcefile = form.sourcefile.data
         sheetname = form.sheetname.data
         colname = form.colname.data
@@ -32,8 +35,8 @@ def start():
         destfile = form.newFile_dest.data
         destfilesheet = form.newSheetName.data
         op_file = pd.read_excel(sourcefile,sheetname)
-        sorted_data = op_file.sort_values(colname,ascending = (sortingorder))
-        try:
+        sorted_data = op_file.sort_values(colname,ascending = (sortingorder)) ##Data sorted
+        try:#trying to push data to new file
             sorted_data.to_excel(destfile,sheet_name = destfilesheet)
             my_file = Path(destfile)
             if(my_file.is_file):
@@ -41,30 +44,6 @@ def start():
         except Exception as e:
             raise e
     return render_template('start.html',form = form)
-    # try:
-    #     if (request.method == "POST"):
-    #         sourcefile = request.form['sourcefile']
-    #         sheetname = request.form['sheetname']
-    #         colname = request.form['colname']
-    #         sortingorder = request.form['sortingorder']
-    #         destfile = request.form['destfile']
-    #         destfilesheet = request.form['destfilesheet']
-    #         user_file = sourcefile
-    #         sheet_name = sheetname
-    #         op_file = pd.read_excel(user_file,sheet_name)
-    #         #col_numbers = # later it can be incresed.
-    #         col_name = colname
-    #         sort_order = sortingorder
-    #         sorted_data = op_file.sort_values(col_name,ascending = (sort_order))
-    #         newFile_dest = destfile
-    #         newSheetName = destfilesheet
-    #         sorted_data.to_excel(newFile_dest,sheet_name = newSheetName)
-            
-    # except Exception as e:
-    #     flash(e)
-    #     return render_template('/start.html')              
-
-    # return render_template('/start.html')
 
 @app.route('/about')
 def about():
@@ -75,6 +54,6 @@ def finished():
     return render_template('finished.html')
 
 if __name__ == '__main__':
-    app.secret_key = 'super secret key'
+    app.secret_key = 'super secret key' #secrent key init
     app.debug = True
     app.run()
